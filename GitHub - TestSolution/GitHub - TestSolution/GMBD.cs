f8b611d1cf6b3cc8a6f6095a04abab7ac4ff55ba
Maxime <Maxime.Michel.23@hotmail.com>
2018-10-12 16:02:10 +0200

    Ajout de la référence de PDSGBD et de l'usine factory

---
+++ GitHub - TestSolution/GitHub - TestSolution/GMBD.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PDSGBD;
+
+namespace GitHub___TestSolution
+{
+    class GMBD
+    {
+
+        /// <summary>
+        /// Référence l'objet de connexion au serveur de base de données MySql
+        /// </summary>
+        private MyDB m_BD;
+
+        /// <summary>
+        /// Référence de l'objet de connexion au serveur MySQL
+        /// </summary>
+        public MyDB BD
+        {
+            get
+            {
+                return m_BD;
+            }
+        }
+
+        /// <summary>
+        /// Constructeur par défaut
+        /// </summary>
+        public GMBD()
+        {
+            m_BD = new MyDB("u_admin_balademoto", "rctODuw74ZZuGtkY", "examen_michel_maxime_pdsgbd");
+            m_BD.SurErreur += (ConnexionEmettrice, MethodeEmettrice, RequeteSql, Valeurs, MessageErreur) =>
+            {
+                System.Diagnostics.Debug.WriteLine(string.Format("\nERREUR SQL :\nMéthode : {0}\nRequête initiale : {1}\nValeurs des {2} parties variables : {3}\nRequête exécutée : {4}\nMessage d'erreur : {5}\n",
+                    MethodeEmettrice,
+                    RequeteSql,
+                    (Valeurs != null) ? Valeurs.Length : 0,
+                    ((Valeurs != null) && (Valeurs.Length >= 1)) ? "\n* " + string.Join("\n* ", Valeurs.Select((Valeur, Indice) => string.Format("Valeurs[{0}] : {1}", Indice, (Valeur != null) ? Valeur.ToString() : "NULL")).ToArray()) : string.Empty,
+                    MyDB.FormaterEnSql(RequeteSql, Valeurs),
+                    MessageErreur));
+            };
+        }
+
+        /// <summary>
+        /// Permet d'initialiser ce gestionnaire de modèles
+        /// </summary>
+        /// <returns></returns>
+        public bool Initialiser()
+        {
+            return m_BD.SeConnecter();
+        }
+
+
+
+
+        public Utilisateur ConnexionApplication(string Pseudo, string MotDePasse)
+        {
+            return EnumererUtilisateur(null,
+                                       new MyDB.CodeSql(@"JOIN role ON utilisateur.ref_role = id_role"),
+                                       new MyDB.CodeSql("WHERE pseudo = {0} AND mot_de_passe = SHA1({1})", Pseudo, MotDePasse),
+                                       null).FirstOrDefault();
+        }
+
+    }
+}
