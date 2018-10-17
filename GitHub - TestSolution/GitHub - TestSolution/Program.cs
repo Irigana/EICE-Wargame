fa72eb57dca60983a903e7a8c030d38df7528a20
Maxime <Maxime.Michel.23@hotmail.com>
2018-10-17 17:40:50 +0200

    Ajout du dossier des modèles et du modèle Entite, Ajout du dossier Outils ainsi que les fonctions faites par le prof ( va avec le modèle d'entite )

--- GitHub - TestSolution/GitHub - TestSolution/Program.cs
+++ GitHub - TestSolution/GitHub - TestSolution/Program.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using PDSGBD;
 
 namespace EICE__WARGAME
 {
     static class Program
     {
+
+        private static GMBD s_GMBD;
+
+        public static GMBD GMBD
+        {
+            get
+            {
+                return s_GMBD;
+            }
+        }
+
         /// <summary>
         /// Point d'entrée principal de l'application.
         /// </summary>
@@ -17,6 +29,37 @@ namespace EICE__WARGAME
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form_Principal());
+            /*
+            s_GMBD = new GMBD();
+            s_GMBD.BD.SurChangementEtatConnexion += BD_SurChangementEtatConnexion;
+            if (!s_GMBD.Initialiser())
+            {
+                MessageBox.Show("Erreur d'accès à la base de données !", "Balade moto", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+                return;
+            }
+            using (Connexion Connexion = new Connexion())
+            {
+                Application.Run(Connexion);
+                if (Connexion.EstIdentifie)
+                {
+                    Application.Run(new FormGestion(Connexion.Utilisateur));
+                }
+            }
+
+
+        }
+
+
+        /// <summary>
+        /// Méthode appelée lorsqu'un changement d'état de la connexion au serveur MySQL se produit
+        /// </summary>
+        /// <param name="ConnexionEmetrice">Connexion concernée par le changement d'état</param>
+        /// <param name="EtatPrecedent">Etat précédant ce changement</param>
+        /// <param name="NouvelEtat">Nouvel état résultant de ce changement</param>
+        private static void BD_SurChangementEtatConnexion(MyDB ConnexionEmetrice, MyDB.EtatConnexion EtatPrecedent, MyDB.EtatConnexion NouvelEtat)
+        {
+            System.Diagnostics.Debug.WriteLine(string.Format("\nCONNEXION BD :\nChangement d'état : {0} ==>> {1}\n", EtatPrecedent, NouvelEtat));
+        }*/
         }
     }
 }
