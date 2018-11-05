2b818eb95f2039461b5900197da1afa678c2ecbf
Maxime <Maxime.Michel.23@hotmail.com>
2018-11-05 14:35:57 +0100

    Ajout d'un dossier pour les listes déroulantes

---
+++ GitHub - TestSolution/GitHub - TestSolution/Vues/Pages/PageMaCollection.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace EICE_WARGAME
+{
+    partial class PageMaCollection
+    {
+        /// <summary> 
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur de composants
+
+        /// <summary> 
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas 
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelMaCollection = new System.Windows.Forms.Label();
+            this.buttonReturn = new System.Windows.Forms.Button();
+            this.panelLigneSeparatrice = new System.Windows.Forms.Panel();
+            this.labelFaction = new System.Windows.Forms.Label();
+            this.labelSousFaction = new System.Windows.Forms.Label();
+            this.labelUnity = new System.Windows.Forms.Label();
+            this.labelFigurine = new System.Windows.Forms.Label();
+            this.labelRang = new System.Windows.Forms.Label();
+            this.labelEquipement = new System.Windows.Forms.Label();
+            this.labelQuantite = new System.Windows.Forms.Label();
+            this.labelMesFigurines = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelMaCollection
+            // 
+            this.labelMaCollection.AutoSize = true;
+            this.labelMaCollection.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F);
+            this.labelMaCollection.Location = new System.Drawing.Point(23, 67);
+            this.labelMaCollection.Name = "labelMaCollection";
+            this.labelMaCollection.Size = new System.Drawing.Size(236, 42);
+            this.labelMaCollection.TabIndex = 0;
+            this.labelMaCollection.Text = "Ma collection";
+            // 
+            // buttonReturn
+            // 
+            this.buttonReturn.FlatAppearance.BorderSize = 0;
+            this.buttonReturn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonReturn.Image = global::EICE_WARGAME.Properties.Resources.ReturnLogo35px;
+            this.buttonReturn.Location = new System.Drawing.Point(3, 3);
+            this.buttonReturn.Name = "buttonReturn";
+            this.buttonReturn.Size = new System.Drawing.Size(44, 40);
+            this.buttonReturn.TabIndex = 1;
+            this.buttonReturn.UseVisualStyleBackColor = true;
+            this.buttonReturn.Click += new System.EventHandler(this.buttonReturn_Click);
+            // 
+            // panelLigneSeparatrice
+            // 
+            this.panelLigneSeparatrice.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panelLigneSeparatrice.Location = new System.Drawing.Point(30, 112);
+            this.panelLigneSeparatrice.Name = "panelLigneSeparatrice";
+            this.panelLigneSeparatrice.Size = new System.Drawing.Size(1400, 3);
+            this.panelLigneSeparatrice.TabIndex = 2;
+            // 
+            // labelFaction
+            // 
+            this.labelFaction.AutoSize = true;
+            this.labelFaction.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.labelFaction.Location = new System.Drawing.Point(62, 164);
+            this.labelFaction.Name = "labelFaction";
+            this.labelFaction.Size = new System.Drawing.Size(64, 20);
+            this.labelFaction.TabIndex = 3;
+            this.labelFaction.Text = "Faction";
+            // 
+            // labelSousFaction
+            // 
+            this.labelSousFaction.AutoSize = true;
+            this.labelSousFaction.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.labelSousFaction.Location = new System.Drawing.Point(62, 205);
+            this.labelSousFaction.Name = "labelSousFaction";
+            this.labelSousFaction.Size = new System.Drawing.Size(102, 20);
+            this.labelSousFaction.TabIndex = 4;
+            this.labelSousFaction.Text = "Sous faction";
+            // 
+            // labelUnity
+            // 
+            this.labelUnity.AutoSize = true;
+            this.labelUnity.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.labelUnity.Location = new System.Drawing.Point(62, 246);
+            this.labelUnity.Name = "labelUnity";
+            this.labelUnity.Size = new System.Drawing.Size(48, 20);
+            this.labelUnity.TabIndex = 5;
+            this.labelUnity.Text = "Unité";
+            // 
+            // labelFigurine
+            // 
+            this.labelFigurine.AutoSize = true;
+            this.labelFigurine.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.labelFigurine.Location = new System.Drawing.Point(62, 284);
+            this.labelFigurine.Name = "labelFigurine";
+            this.labelFigurine.Size = new System.Drawing.Size(69, 20);
+            this.labelFigurine.TabIndex = 6;
+            this.labelFigurine.Text = "Figurine";
+            // 
+            // labelRang
+            // 
+            this.labelRang.AutoSize = true;
+            this.labelRang.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.labelRang.Location = new System.Drawing.Point(62, 327);
+            this.labelRang.Name = "labelRang";
+            this.labelRang.Size = new System.Drawing.Size(48, 20);
+            this.labelRang.TabIndex = 7;
+            this.labelRang.Text = "Rang";
+            // 
+            // labelEquipement
+            // 
+            this.labelEquipement.AutoSize = true;
+            this.labelEquipement.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.labelEquipement.Location = new System.Drawing.Point(62, 370);
+            this.labelEquipement.Name = "labelEquipement";
+            this.labelEquipement.Size = new System.Drawing.Size(97, 20);
+            this.labelEquipement.TabIndex = 8;
+            this.labelEquipement.Text = "Equipement";
+            // 
+            // labelQuantite
+            // 
+            this.labelQuantite.AutoSize = true;
+            this.labelQuantite.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.labelQuantite.Location = new System.Drawing.Point(62, 465);
+            this.labelQuantite.Name = "labelQuantite";
+            this.labelQuantite.Size = new System.Drawing.Size(72, 20);
+            this.labelQuantite.TabIndex = 9;
+            this.labelQuantite.Text = "Quantité";
+            // 
+            // labelMesFigurines
+            // 
+            this.labelMesFigurines.AutoSize = true;
+            this.labelMesFigurines.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.labelMesFigurines.Location = new System.Drawing.Point(664, 154);
+            this.labelMesFigurines.Name = "labelMesFigurines";
+            this.labelMesFigurines.Size = new System.Drawing.Size(110, 20);
+            this.labelMesFigurines.TabIndex = 10;
+            this.labelMesFigurines.Text = "Mes figurines";
+            // 
+            // PageMaCollection
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.labelMesFigurines);
+            this.Controls.Add(this.labelQuantite);
+            this.Controls.Add(this.labelEquipement);
+            this.Controls.Add(this.labelRang);
+            this.Controls.Add(this.labelFigurine);
+            this.Controls.Add(this.labelUnity);
+            this.Controls.Add(this.labelSousFaction);
+            this.Controls.Add(this.labelFaction);
+            this.Controls.Add(this.panelLigneSeparatrice);
+            this.Controls.Add(this.buttonReturn);
+            this.Controls.Add(this.labelMaCollection);
+            this.Name = "PageMaCollection";
+            this.Size = new System.Drawing.Size(1500, 750);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelMaCollection;
+        private System.Windows.Forms.Button buttonReturn;
+        private System.Windows.Forms.Panel panelLigneSeparatrice;
+        private System.Windows.Forms.Label labelFaction;
+        private System.Windows.Forms.Label labelSousFaction;
+        private System.Windows.Forms.Label labelUnity;
+        private System.Windows.Forms.Label labelFigurine;
+        private System.Windows.Forms.Label labelRang;
+        private System.Windows.Forms.Label labelEquipement;
+        private System.Windows.Forms.Label labelQuantite;
+        private System.Windows.Forms.Label labelMesFigurines;
+    }
+}
