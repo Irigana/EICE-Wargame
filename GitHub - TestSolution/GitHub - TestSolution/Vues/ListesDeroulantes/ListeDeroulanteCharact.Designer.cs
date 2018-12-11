49b5af6c474c568451429c9f151a04db61f707fe
Iziel Irigana <jason.brognon@gmail.com>
2018-12-11 13:08:23 +0100

    Création de la liste déroulante Charact

---
+++ GitHub - TestSolution/GitHub - TestSolution/Vues/ListesDeroulantes/ListeDeroulanteCharact.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace EICE_WARGAME
+{
+    partial class ListeDeroulanteChar
+    {
+        /// <summary> 
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur de composants
+
+        /// <summary> 
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas 
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxChar = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // comboBoxChar
+            // 
+            this.comboBoxChar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.comboBoxChar.FormattingEnabled = true;
+            this.comboBoxChar.Location = new System.Drawing.Point(0, 0);
+            this.comboBoxChar.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBoxChar.Name = "comboBoxChar";
+            this.comboBoxChar.Size = new System.Drawing.Size(195, 21);
+            this.comboBoxChar.TabIndex = 0;
+            // 
+            // ListeDeroulanteChar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.comboBoxChar);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "ListeDeroulanteChar";
+            this.Size = new System.Drawing.Size(195, 24);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxChar;
+    }
+}
---
+++ GitHub - TestSolution/GitHub - TestSolution/Vues/ListesDeroulantes/ListeDeroulanteCharact.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EICE_WARGAME
+{
+    public partial class ListeDeroulanteChar : UserControl
+    {
+        public ListeDeroulanteChar()
+        {
+            InitializeComponent();
+            this.SizeChanged += ListeDeroulanteCharRank_SizeChanged;
+            comboBoxChar.SelectedIndexChanged += ComboCharact_SelectedIndexChanged;
+        }
+        
+        private class Element
+        {
+
+            public Charact Charact { get; private set; }
+
+            public Element(Charact Charact)
+            {
+                this.Charact = Charact;
+            }
+
+            public override string ToString()
+            {
+                return string.Format("{0}",Charact.Name);
+            }
+
+        }
+        
+
+        public IEnumerable<Charact> Charact
+        {
+            get
+            {
+                return comboBoxChar.Items.OfType<Element>().Select(Element => Element.Charact);
+            }
+            set
+            {
+                if (value != null)
+                {
+                    comboBoxChar.Items.Clear();
+                    foreach (Charact Charact in value)
+                    {
+                        comboBoxChar.Items.Add(new Element(Charact));
+                    }
+                }
+            }
+        }
+        
+        public int SelectedIndex(int Index)
+        {
+            return comboBoxChar.SelectedIndex = Index - 1;
+        }
+
+        public Charact CharactSelectionnee
+        {
+            get
+            {
+                return (comboBoxChar.SelectedItem is Element) ? (comboBoxChar.SelectedItem as Element).Charact : null;
+            }
+            set
+            {
+                comboBoxChar.SelectedItem = (value != null) ? new Element(value) : null;
+            }
+        }
+
+        public EventHandler SurChangementSelection = null;
+
+        private void ComboCharact_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (SurChangementSelection != null)
+            {
+                SurChangementSelection(this, EventArgs.Empty);
+            }
+        }
+
+        private void ListeDeroulanteCharRank_SizeChanged(object sender, EventArgs e)
+        {
+            this.Size = new Size(this.Size.Width, comboBoxChar.Height);
+        }
+    }
+}
