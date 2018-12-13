ef12c537264365c46b37a133aa53bdc9fa1a9fd7
Irigana <jason.brognon@gmail.com>
2018-12-13 23:05:03 +0100

    Test de l'impressions sur ma collection ( c'est juste un test en vue de la vrai impressions

--- GitHub - TestSolution/GitHub - TestSolution/Vues/Pages/PageMaCollection.Designer.cs
+++ GitHub - TestSolution/GitHub - TestSolution/Vues/Pages/PageMaCollection.Designer.cs
@@ -54,6 +54,7 @@
             this.listeDeroulanteSousFaction1 = new EICE_WARGAME.ListeDeroulanteSousFaction();
             this.listeDeroulanteFaction1 = new EICE_WARGAME.ListeDeroulanteFaction();
             this.buttonOptionsUser1 = new EICE_WARGAME.ButtonOptionsUser();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // labelMaCollection
@@ -334,10 +335,21 @@
             this.buttonOptionsUser1.TabIndex = 11;
             this.buttonOptionsUser1.Utilisateur = null;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(22, 210);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 50;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.printButton_Click);
+            // 
             // PageMaCollection
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.buttonAnnulerPersonnage);
             this.Controls.Add(this.buttonSupprimerPersonnage);
@@ -401,5 +413,6 @@
         private System.Windows.Forms.Button buttonModifierPersonnage;
         private System.Windows.Forms.Button buttonAjouterFigurine;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
