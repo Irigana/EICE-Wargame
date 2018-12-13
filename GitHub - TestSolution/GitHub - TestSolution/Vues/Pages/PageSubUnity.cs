ad25f6969b244376673f25cd68c2466bb43e6430
Maxime <Maxime.Michel.23@hotmail.com>
2018-12-13 22:49:08 +0100

    Nouvelle page sous unité

---
+++ GitHub - TestSolution/GitHub - TestSolution/Vues/Pages/PageSubUnity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EICE_WARGAME.Vues.Pages
+{
+    public partial class PageSubUnity : UserControl
+    {
+        public PageSubUnity()
+        {
+            InitializeComponent();
+        }
+    }
+}
