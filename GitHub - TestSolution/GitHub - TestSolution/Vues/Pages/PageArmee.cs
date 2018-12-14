02013348f865c7f75a39e31f9061d9a244e47957
guillaume toller <tollerguillaume1@hotmail.com>
2018-12-14 21:20:28 +0100

    Ajout d'une liste déroulante de character

--- GitHub - TestSolution/GitHub - TestSolution/Vues/Pages/PageArmee.cs
+++ GitHub - TestSolution/GitHub - TestSolution/Vues/Pages/PageArmee.cs
@@ -42,15 +42,19 @@ namespace EICE_WARGAME
             z_listeDeroulanteFigurine.Figurine = Program.GMBD.EnumererFigurine(null, null,
                 new MyDB.CodeSql("WHERE fi_fk_user_id = 22"), null);
 
-            z_listeDeroulanteFigurine.SurChangementSelection += Figurine_SurChangementSelection;
+            z_listeDeroulanteChar.Charact = Program.GMBD.EnumererCaractere(new MyDB.CodeSql("DISTINCT ch_id, ch_name, ch_fk_subfaction_id"),
+                new MyDB.CodeSql("JOIN figurine ON fi_fk_character_id"), new MyDB.CodeSql("WHERE fi_fk_user_id = {0}", MonUser),
+                new MyDB.CodeSql("ORDER BY ch_name"));
+            z_listeDeroulanteChar.SurChangementSelection += Charact_surChangementSelection;
+            //z_listeDeroulanteFigurine.SurChangementSelection += Figurine_SurChangementSelection;
         }
 
-        private void Figurine_SurChangementSelection(object sender, EventArgs e)
+        private void Charact_surChangementSelection(object sender, EventArgs e)
         {
             z_listeDeroulanteStuff.Stuff = Program.GMBD.EnumererStuff(null,
                 new MyDB.CodeSql(@"JOIN figurine_stuff ON stuff.st_id = figurine_stuff.fs_fk_stuff_id
                                     JOIN figurine ON figurine_stuff.fs_fk_figurine_id = figurine.fi_id AND figurine.fi_fk_user_id = 22
-                                    JOIN charact ON charact.ch_id = figurine.fi_fk_character_id"),null,null);
+                                    JOIN charact ON charact.ch_id = figurine.fi_fk_character_id AND ch_id = {0}", z_listeDeroulanteChar.CharactSelectionnee),null,null);
         }
         
     }
